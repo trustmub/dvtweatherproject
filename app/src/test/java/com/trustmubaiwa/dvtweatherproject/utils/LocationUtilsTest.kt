ec5c12338be50f25af995cfa9db5f16f73dc0b53
Trust Mubaiwa <trust.mabaiwa@standardbank.co.za>
2021-08-11 13:18:23 +0200

    local utils tests

--- app/src/test/java/com/trustmubaiwa/dvtweatherproject/utils/LocationUtilsTest.kt
+++ app/src/test/java/com/trustmubaiwa/dvtweatherproject/utils/LocationUtilsTest.kt
@@ -1,13 +1,37 @@
 package com.trustmubaiwa.dvtweatherproject.utils
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import junit.framework.TestCase
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 class LocationUtilsTest : TestCase() {
 
+    private lateinit var locationUtils: LocationUtils
+
+    @Mock
+    private lateinit var mockDataStore: DataStore<Preferences>
+
+    @Mock
+    private lateinit var mockContext: Context
+
+    @Before
+    override fun setUp() {
+        super.setUp()
+        MockitoAnnotations.initMocks(this)
+
+        runBlocking {
+            locationUtils = LocationUtils(mockDataStore, mockContext)
+        }
+    }
 
     @Test
-    fun `test `() {
-
+    fun `test true`() {
+        assertTrue(true)
     }
 }
